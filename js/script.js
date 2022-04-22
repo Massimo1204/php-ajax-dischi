89ec2254dca135626be1329a236860f87f2497b9
Massimo1204 <xiangmassimo@gmail.com>
2022-04-22 17:20:27 +0200

    half cards

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ const App = new Vue({
         axios.get("http://localhost/php-ajax-dischi/server/controller.php").then(
             (result) => {
                 this.albums = result.data.results;
-                console.log(this.albums);
             }
         ).catch((error) => {
             console.error(error);
--- partials/main.php
+++ partials/main.php
@@ -1,4 +1,12 @@
 <main>
-    <div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-2 text-center" v-for="(element,index) in albums">
+                <div class="img-wrapper">
+                    <img class="img-fluid" :src="element.poster" alt="element.title">
+                </div>
+                <h4 class="text-light text-uppercase"> {{ element.title }}</h4>
+            </div>
+        </div>
     </div>
 </main>
