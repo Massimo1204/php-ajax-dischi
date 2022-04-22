a1f0d12f242a56a2d686ade3022d102458173953
Massimo1204 <xiangmassimo@gmail.com>
2022-04-22 16:23:35 +0200

    added datas

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,4 @@
 <header>
     <h1 class="text-danger text-uppercase text-center">ciaociia</h1>
+    <img src="https:\/\/www.onstageweb.com\/wp-content\/uploads\/2018\/09\/bon-jovi-new-jersey.jpg" alt="">
 </header>
